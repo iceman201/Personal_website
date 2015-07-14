d7970e7b406cbcd7bdd6057dd2f1674e3b27a016
Liguo Jiao <jiaoliguo888@gmail.com>
2015-07-15 02:28:15 +1200

    First_version
    Contact form doesnt work perfectly, setup every framework, and picture
    from online, will go deeper editing in the future.

---
+++ index.php
@@ -0,0 +1,448 @@
+<!--
+
+Name: Leandro Jiao (Liguo Jiao)
+University of Canterbury
+
+Personal Website
+Individual creation
+
+-->
+<!DOCTYPE html>
+
+<html lang="en">
+<head>
+	<meta charset="utf-8">
+	<title>Leandro Jiao</title>
+	<meta http-equiv="X-UA-Compatible" content="IE=Edge">
+	<meta name="viewport" content="width=device-width, initial-scale=1">
+	<meta name="keywords" content="">
+	<meta name="description" content="">
+
+	<link href='http://fonts.googleapis.com/css?family=Montserrat:400,700' rel='stylesheet' type='text/css'>
+	<link rel="stylesheet" href="css/animate.min.css">
+	<link rel="stylesheet" href="css/bootstrap.min.css">
+	<link rel="stylesheet" href="css/font-awesome.min.css">		
+	<link rel="stylesheet" href="css/web-style.css">
+</head>
+
+<body data-spy="scroll" data-offset="50" data-target=".navbar-collapse">
+	<div class="preloader">
+		<div class="sk-spinner sk-spinner-rotating-plane"></div>
+	</div>
+	<!--Bootstrap Navigation bar-->
+	<nav class="navbar navbar-fixed-top custom-navbar" role="navigation">
+		<div class="container">
+			<div class="navbar-header">
+				<button class="navbar-toggle" data-toggle="collapse" data-target=".navbar-collapse">
+					<span class="icon icon-bar"></span>
+					<span class="icon icon-bar"></span>
+					<span class="icon icon-bar"></span>
+				</button>
+				<a href="#" class="navbar-brand">Leandro Jiao</a>
+			</div>
+
+			<div class="collapse navbar-collapse"> 
+				<ul class="nav navbar-nav navbar-right"> <!-- setup navbar position-->
+					<li><a href="#home" class="smoothScroll">Home</a></li>
+					<li><a href="#service" class="smoothScroll">Services</a></li>
+					<li><a href="#about" class="smoothScroll">About</a></li>
+					<li><a href="#portfolio" class="smoothScroll">Portfolio</a></li>
+					<li><a href="#contact" class="smoothScroll">Contact</a></li>
+				</ul>
+			</div>
+		</div>
+	</nav>
+
+	<!-- home -->
+	<section id="home">
+		<div class="overlay">
+			<div class="flexslider">
+				<ul class="slides">
+					<li>
+						<img src="images/slider/1.jpg" alt="Slide 1">
+						<div class="slider-caption">
+							<div class="webmo_homewrapper">
+								<h3 class="wow bounceIn">web design and development</h3>
+								<h1 class="wow bounce">Digital Studio</h1>
+								<h2>
+									<span class="wow fadeIn" data-wow-delay="0.3s">WEB,</span>
+									<span class="wow fadeIn" data-wow-delay="0.6s">MOBILE,</span>
+									<span class="wow fadeIn" data-wow-delay="0.9s">UX DESIGN</span>
+								</h2>
+								<a href="#portfolio" class="smoothScroll webmo-slider-btn btn btn-default">Learn More</a>
+							</div>
+						</div>
+					</li>
+					<li>
+						<img src="images/slider/2.jpg" alt="Slide 2">
+						<div class="slider-caption">
+							<div class="webmo_homewrapper">
+                            	<h2>CSS Flexbox</h2>
+								<h1>Flex Slider</h1>
+								<h3>Work on all modern browsers, IE 10+</h3>
+								<a href="#about" class="smoothScroll webmo-slider-btn btn btn-default">Meet Our Team</a>	
+							</div>
+						</div>
+					</li>
+				</ul>
+			</div>
+		</div>
+	</section>
+	<!-- end -->
+
+	<!-- service -->
+	<section id="service">
+		<div class="container">
+			<div class="row">
+
+				<div class="col-md-12 text-center wow bounceIn">
+					<h2>Services</h2>
+					<hr>
+					<h4>we specialize in web and mobile</h4>
+				</div>
+
+				<div class="col-md-6 col-sm-6">
+					<div class="media">
+						<div class="media-object media-left wow fadeIn" data-wow-delay="0.6s">
+							<i class="fa fa-laptop"></i>
+						</div>
+						<div class="media-body">
+							<h3 class="media-heading">Web Design</h3>
+							<p>Create website for E-Commerce using.</p>
+						</div>
+					</div>
+				</div>
+
+				<div class="col-md-6 col-sm-6">
+					<div class="media">
+						<div class="media-object media-left wow fadeIn" data-wow-delay="0.6s">
+							<i class="fa fa-apple"></i>
+						</div>
+						<div class="media-body">
+							<h3 class="media-heading">Mobile App</h3>
+							<p>Focus on IOS development, mainly programming in Swift</p>
+						</div>
+					</div>
+				</div>
+
+				<div class="col-md-6 col-sm-6">
+					<div class="media">
+						<div class="media-object media-left wow fadeIn" data-wow-delay="0.6s">
+							<i class="fa fa-camera"></i>
+						</div>
+						<div class="media-body">
+							<h3 class="media-heading">Photography</h3>
+							<p>Events, Art Portrait Photography, Advertising Photography.</p>
+						</div>
+					</div>
+				</div>
+
+				<div class="col-md-6 col-sm-6">
+					<div class="media">
+						<div class="media-object media-left wow fadeIn" data-wow-delay="0.6s">
+							<i class="fa fa-html5"></i>
+						</div>
+						<div class="media-body">
+							<h3 class="media-heading">HTML5 &amp; CSS3 PHP JavaScript</h3>
+							<p>This is based on Bootstrap v3.3.4 layout. </p>
+						</div>
+					</div>
+				</div>
+
+			</div>
+		</div>
+	</section>
+	<!-- end -->
+
+	<!-- divider -->
+	<div class="divider">
+		<div class="overlay">
+			<div class="container">
+				<div class="row">
+					<div class="divider-des">
+						<h3 class="text-uppercase">The best minimal business one page ever</h3>
+						<p>It is fully responsive, clean, modern, creative, and minimal.</p>
+						<div class="btn-group-vertical">
+							<button type = "button1" class="btn btn-default text-uppercase">Download</button>
+							<button type = "button2" class="btn btn-info">Haha</button>
+							<button type = "button3" class="btn btn-info">Hehe</button>
+							<button type = "button4" class="btn btn-info">Lala</button>
+							<button type = "button5" class="btn btn-info">Wawa</button>
+						</div>
+					</div>
+				</div>
+			</div>
+		</div>
+	</div>
+	<!-- end -->
+
+	<!-- about -->
+	<section id="about">
+		<div class="container">
+			<div class="row">
+
+				<div class="col-md-12 wow bounceIn">
+					<h2>About</h2>
+					<hr>
+					<h4>a little about our company...</h4>
+				</div>
+
+			</div>
+			<div class="row">
+				<div class="col-md-6 col-sm-6 wow fadeInLeft webmo-about-left" data-wow-delay="0.3s">
+					<h3 class="about-title">Studio History</h3>
+					<p>Phasellus tempus malesuada congue. Sed id est tincidunt, iaculis nulla vel, sodales metus. Morbi interdum accumsan augue, in accumsan neque lacinia sed. Fusce cursus eu ligula ut gravida. Ut tristique hendrerit bibendum. Suspendisse euismod sit amet quam et luctus.</p>
+                    <p>Nulla viverra diam sed justo vehicula, non venenatis massa gravida. Nunc efficitur et quam non tempor. Nullam nec nisl eget est ultrices elementum nec vel nisl. Cras vestibulum nisl vel justo tempor, ut lacinia est accumsan.</p>
+				</div>
+				<div class="col-md-6 col-sm-6 wow fadeInRight" data-wow-delay="0.3s">
+
+					<span class="text-uppercase">BOOTSTRAP 100%</span>
+
+					<div class="progress">
+						<div class="progress-bar progress-bar-danger" role="progressbar" aria-valuenow="60" aria-valuemin="0" aria-valuemax="100" style="width: 100%;"></div>
+					</div>
+
+					<span class="text-uppercase">HTML5 &AMP; CSS3 90%</span>
+					<div class="progress">
+						<div class="progress-bar progress-bar-danger" role="progressbar" aria-valuenow="90" aria-valuemin="0" aria-valuemax="100" style="width: 90%;"></div>
+					</div>
+
+					<span class="text-uppercase">Wordpress 65%</span>
+					<div class="progress">
+						<div class="progress-bar progress-bar-danger" role="progressbar" aria-valuenow="65" aria-valuemin="0" aria-valuemax="100" style="width: 65%;"></div>
+					</div>
+
+				</div>
+			</div>
+
+			<div class="row">
+
+				<div class="col-md-4 col-sm-4 wow fadeInUp" data-wow-delay="0.6s">
+					<div class="about-wrapper">
+						<img src="images/team-img-1.png" class="img-responsive" alt="about img">
+						<h3>Someone 1</h3>
+						<h5>Founder &amp; Director</h5>
+					</div>
+				</div>
+
+				<div class="col-md-4 col-sm-4 wow fadeInUp" data-wow-delay="0.6s">
+					<div class="about-wrapper">
+						<img src="images/team-img-2.png" class="img-responsive" alt="about img">
+						<h3>Ms.Haha</h3>
+						<h5>Creative Manager</h5>
+					</div>
+				</div>
+
+				<div class="col-md-4 col-sm-4 wow fadeInUp" data-wow-delay="0.6s">
+					<div class="about-wrapper">
+						<img src="images/team-img-3.png" class="img-responsive" alt="about img">
+						<h3>Mr.Japan</h3>
+						<h5>Co-Founder &amp; Designer</h5>
+					</div>
+				</div>
+
+			</div>
+		</div>
+	</section>
+	<!-- end -->
+
+	<!-- portfolio -->
+	<section id="portfolio">
+		<div class="container">
+			<div class="row">
+				<div class="col-md-12">
+					<div class="wow bounceIn">
+						<h2 class="wow bounceIn">Portfolio</h2>
+						<hr>
+						<h4>our lastest projects in the past 6 months</h4>
+					</div>
+					<div class="iso-section wow fadeIn" data-wow-delay="0.6s">
+
+						<ul class="filter-wrapper clearfix">
+							<li><a href="#" data-filter="*" class="selected opc-main-bg">All</a></li>
+							<li><a href="#" class="opc-main-bg" data-filter=".graphic">Graphic</a></li>
+							<li><a href="#" class="opc-main-bg" data-filter=".photoshop">Photoshop</a></li>
+							<li><a href="#" class="opc-main-bg" data-filter=".wallpaper">Wallpaper</a></li>
+						</ul>
+
+						<div class="iso-box-section">
+							<div class="iso-box-wrapper col4-iso-box">
+
+								<div class="iso-box graphic photoshop wallpaper col-md-4 col-sm-6 col-xs-12">
+									<div class="portfolio-thumb">
+										<img src="images/portfolio-img-1.jpg" class="fluid-img" alt="portfolio img">
+										<div class="portfolio-overlay">
+											<div class="portfolio-description">
+												<a href="#" class="fa fa-search"></a>
+												<a href="#" class="fa fa-link"></a>
+												<h4>Project title</h4>
+												<p>Lorem ipsum dolor sit amet, consectetuer adipiscing elit, sed diam nonumm.</p>	
+											</div>											
+										</div>
+									</div>
+								</div>
+
+								<div class="iso-box graphic wallpaper col-md-4 col-sm-6 col-xs-12">
+									<div class="portfolio-thumb">
+										<img src="images/portfolio-img-2.jpg" class="fluid-img" alt="portfolio img">
+										<div class="portfolio-overlay">
+											<div class="portfolio-description">
+												<a href="#" class="fa fa-search"></a>
+												<a href="#" class="fa fa-link"></a>
+												<h4>Project title</h4>
+												<p>Lorem ipsum dolor sit amet, consectetuer adipiscing elit, sed diam nonumm.</p>
+											</div>
+										</div>
+									</div>
+								</div>
+
+								<div class="iso-box wallpaper col-md-4 col-sm-6 col-xs-12">
+									<div class="portfolio-thumb">
+										<img src="images/portfolio-img-3.jpg" class="fluid-img" alt="portfolio img">
+										<div class="portfolio-overlay">
+											<div class="portfolio-description">
+												<a href="#" class="fa fa-search"></a>
+												<a href="#" class="fa fa-link"></a>
+												<h4>Project title</h4>
+												<p>Lorem ipsum dolor sit amet, consectetuer adipiscing elit, sed diam nonumm.</p>
+											</div>
+										</div>
+									</div>
+								</div>
+
+								<div class="iso-box graphic col-md-4 col-sm-6 col-xs-12">
+									<div class="portfolio-thumb">
+										<img src="images/portfolio-img-4.jpg" class="fluid-img" alt="portfolio img">
+										<div class="portfolio-overlay">
+											<div class="portfolio-description">
+												<a href="#" class="fa fa-search"></a>
+												<a href="#" class="fa fa-link"></a>
+												<h4>Project title</h4>
+												<p>Lorem ipsum dolor sit amet, consectetuer adipiscing elit, sed diam nonumm.</p>
+											</div>
+										</div>
+									</div>
+								</div>
+
+								<div class="iso-box wallpaper col-md-4 col-sm-6 col-xs-12">
+									<div class="portfolio-thumb">
+										<img src="images/portfolio-img-5.jpg" class="fluid-img" alt="portfolio img">
+										<div class="portfolio-overlay">
+											<div class="portfolio-description">
+												<a href="#" class="fa fa-search"></a>
+												<a href="#" class="fa fa-link"></a>
+												<h4>Project title</h4>
+												<p>Lorem ipsum dolor sit amet, consectetuer adipiscing elit, sed diam nonumm.</p>
+											</div>
+										</div>
+									</div>
+								</div>
+								<div class="iso-box graphic photoshop col-md-4 col-sm-6 col-xs-12">
+									<div class="portfolio-thumb">
+										<img src="images/portfolio-img-6.jpg" class="fluid-img" alt="portfolio img">
+										<div class="portfolio-overlay">
+											<div class="portfolio-description">
+												<a href="#" class="fa fa-search"></a>
+												<a href="#" class="fa fa-link"></a>
+												<h4>Project title</h4>
+												<p>Lorem ipsum dolor sit amet, consectetuer adipiscing elit, sed diam nonumm.</p>
+											</div>
+										</div>
+									</div>
+								</div>
+							</div>
+						</div>
+					</div>
+				</div>
+			</div>
+		</div>
+	</section>
+	<!-- end -->
+
+
+	<!-- contact -->
+	<section id="contact">
+		<div class="container">
+			<div class="row">
+				<div class="col-md-12">
+					<div class="wow bounceIn">
+						<h2 class="wow bounceIn">Contact</h2>
+						<hr>
+						<h4>Talk to me :)</h4>
+					</div>
+					<?php
+						if (isset($_POST['contact_name']) && isset($_POST['contact_subject']) && isset($_POST['contact_email']) && isset($_POST['contact_text'])){
+							$contact_name = $_POST['contact_name'];
+							$contact_email = $_POST['contact_email'];
+							$contact_text = $_POST['contact_text'];
+							$contact_subject = $_POST['contact_subject'];
+							if (!empty($contact_text) && !empty($contact_subject) && !empty($contact_email) && !empty($contact_name)){
+								$to = 'jiaoliguo@yahoo.co.nz';
+								$subject = $contact_subject;
+								$content = $contact_name."\n".$contact_text;
+								$headers = 'From:'.$contact_email;
+
+								if (mail($to, $subject, $content, $headers)){
+									echo 'send to jiaoliguo';
+								}
+								else{
+									echo 'Sorry, an error occurred, please try again.';
+								}
+
+							} else {
+								echo 'Please fill up all fields.';
+							}
+						}
+					?>
+					<form action="index.php" method="post" role="form">
+						<div class="col-md-4 col-sm-4 wow fadeIn" data-wow-delay="0.3s">
+							<input type="text" name="contact_name" placeholder="Name" class="form-control">
+						</div>
+						<div class="col-md-4 col-sm-4 wow fadeIn" data-wow-delay="0.3s">
+							<input type="email" name="contact_email" placeholder="Email" class="form-control">
+						</div>
+						<div class="col-md-4 col-sm-4 wow fadeIn" data-wow-delay="0.3s">
+							<input type="text" name="contact_subject" placeholder="Subject" class="form-control">
+						</div>
+						<div class="col-md-12 col-sm-12 wow fadeIn" data-wow-delay="0.9s">
+							<textarea class="form-control" name="contact_text" rows="5" placeholder="Message"></textarea>
+						</div>
+						<div class="col-md-offset-3 col-sm-offset-3 col-sm-6 col-md-6 wow fadeIn" data-wow-delay="0.3s">
+							<input type="submit" value="Send" class="form-control">
+						</div>
+					</form>
+				</div>
+			</div>
+		</div>
+	</section>
+	<!-- end -->
+
+	<!--bottom line-->
+	<footer>
+		<div class="container">
+			<div class="row">
+				<div class="col-md-12">
+					<ul class="social-icon wow fadeIn" data-wow-delay="0.3s">
+						<li><a href="https://www.facebook.com/leandrojiew" class="fa fa-facebook"></a></li>
+						<li><a href="#" class="fa fa-flickr"></a></li>
+						<li><a href="https://instagram.com/leandro_lol/" class="fa fa-instagram"></a></li>
+					</ul>
+					<p class="wow bounceIn">Copyright &copy; <span>Leandro Jiao</span> 15/08/2014 | Website created by: <span>Leandro Jiao</span></p>
+				</div>
+			</div>
+		</div>
+	</footer>
+	<!-- end -->
+
+	<!-- All the script-->
+	<script src="js/jquery.js"></script>
+	<script src="js/bootstrap.min.js"></script>
+	<script src="js/jquery.flexslider.js"></script>
+	<script src="js/isotope.js"></script>
+	<script src="js/imagesloaded.min.js"></script>
+	<script src="js/smoothscroll.js"></script>
+	<script src="js/wow.min.js"></script>
+	<script src="js/custom.js"></script>
+
+</body>
+</html>
