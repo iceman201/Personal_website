a381684ee95b8a1c9f534bc09e6b9f009b7d3cba
Liguo Jiao <jiaoliguo888@gmail.com>
2015-08-07 23:54:43 +1200

    finish photography page

--- index.php
+++ index.php
@@ -256,7 +256,7 @@ Individual creation
 			</div>
 
 			<div class="row">
-
+				<!-- Lee -->
 				<div class="col-md-4 col-sm-4 wow fadeInUp" data-wow-delay="0.6s">
 					<div class="about-wrapper">
 						<img src="images/team-img-2.png" class="img-responsive" alt="about img">
@@ -264,23 +264,23 @@ Individual creation
 						<h5>Founder &amp; Director &amp; Programmer</h5>
 					</div>
 				</div>
-
+				<!-- Danile -->
 				<div class="col-md-4 col-sm-4 wow fadeInUp" data-wow-delay="0.6s">
 					<div class="about-wrapper">
 						<img src="images/team-img-1.png" class="img-responsive" alt="about img">
 						<h3>Daniel Bathurst</h3>
-						<h5>Founder &amp; Techical Manager</h5>
+						<h5>Techical Manager</h5>
 					</div>
 				</div>
-
+				<!-- Can -->
 				<div class="col-md-4 col-sm-4 wow fadeInUp" data-wow-delay="0.6s">
 					<div class="about-wrapper">
 						<img src="images/team-img-3.png" class="img-responsive" alt="about img">
 						<h3>Ms. Lazzzzzy</h3>
-						<h5>Eatting &amp; Sleeping</h5>
+						<h5>Programmer &amp; Sleeping</h5>
 					</div>
 				</div>
-
+				<!-- Cat -->
 				<div class="col-md-4 col-md-push-4 col-sm-4 wow fadeInUp" data-wow-delay="0.6s">
 					<div class="about-wrapper">
 						<img src="images/team-img-3.png" class="img-responsive" alt="about img">
@@ -288,7 +288,6 @@ Individual creation
 						<h5>Eatting &amp; Sleeping</h5>
 					</div>
 				</div>
-
 			</div>
 		</div>
 	</section>
@@ -302,21 +301,21 @@ Individual creation
 					<div class="wow bounceIn">
 						<h2 class="wow bounceIn">Photography</h2>
 						<hr>
-						<h4>our lastest projects in the past 6 months</h4>
+						<h4>People saying that "Photography is an art of observation and it is a form of non-verbal communication." <br>I believe so.</h4>
 					</div>
 					<div class="iso-section wow fadeIn" data-wow-delay="0.6s">
 
 						<ul class="filter-wrapper clearfix">
 							<li><a href="#" data-filter="*" class="selected opc-main-bg">All</a></li>
-							<li><a href="#" class="opc-main-bg" data-filter=".graphic">Photography</a></li>
-							<li><a href="#" class="opc-main-bg" data-filter=".photoshop">Photoshop</a></li>
-							<li><a href="#" class="opc-main-bg" data-filter=".wallpaper">Wallpaper</a></li>
+							<li><a href="#" class="opc-main-bg" data-filter=".Christchurch">Christchurch</a></li>
+							<li><a href="#" class="opc-main-bg" data-filter=".North_Island">North Island</a></li>
+							<li><a href="#" class="opc-main-bg" data-filter=".South_Island">South Island</a></li>
 						</ul>
 				
 						<div class="iso-box-section">
 							<div class="iso-box-wrapper col4-iso-box">
 								<!--1-->
-								<div class="iso-box graphic photoshop wallpaper col-md-4 col-sm-6 col-xs-12">
+								<div class="iso-box South_Island photoshop wallpaper col-md-4 col-sm-6 col-xs-12">
 									<div class="photography-thumb">
 										<img src="https://farm8.staticflickr.com/7486/16253745735_44011305d9_n_d.jpg" class="fluid-img" alt="photography img">
 										<div class="photography-overlay">
@@ -324,13 +323,13 @@ Individual creation
 												<a href="https://farm8.staticflickr.com/7486/16253745735_44011305d9_b_d.jpg" data-lightbox="image-1" class="fa fa-search"></a>
 												<a href="#" class="fa fa-link"></a>
 												<h4>Summer NZ</h4>
-												<p>26/12/2015</p>	
+												<p>26/12/2015.</p>	
 											</div>											
 										</div>
 									</div>
 								</div>
 								<!--JQuery lightbox pop up picture-->
-								<div class="iso-box graphic wallpaper col-md-4 col-sm-6 col-xs-12">
+								<div class="iso-box South_Island wallpaper col-md-4 col-sm-6 col-xs-12">
 									<div class="photography-thumb">
 										<img src="https://farm8.staticflickr.com/7696/17022773400_9ce707305d_n_d.jpg" class="fluid-img" alt="photography img">
 										<div class="photography-overlay">
@@ -338,61 +337,188 @@ Individual creation
 												<a href="https://farm8.staticflickr.com/7696/17022773400_9ce707305d_b_d.jpg" data-lightbox="image-1" class="fa fa-search"></a>
 												<a href="https://www.flickr.com/photos/leandrojiao/17022773400/" class="fa fa-link"></a>
 												<h4>Morning at Rainbow Community</h4>
-												<p>April 2015. Golden Bay Rock climbing trip with UC Rock climbing club</p>
+												<p>April 2015. Golden Bay Rock climbing trip with UC Rock climbing club.</p>
 											</div>
 										</div>
 									</div>
 								</div>
 								<!--3-->
-								<div class="iso-box wallpaper col-md-4 col-sm-6 col-xs-12">
+								<div class="iso-box South_Island col-md-4 col-sm-6 col-xs-12">
 									<div class="photography-thumb">
-										<img src="images/photography/photography-img-3.jpg" class="fluid-img" alt="photography img">
+										<img src="https://farm8.staticflickr.com/7539/15624450653_f2b8eb346a_n_d.jpg" class="fluid-img" alt="photography img">
 										<div class="photography-overlay">
 											<div class="photography-description">
-												<a href="#" data-lightbox="image-1" class="fa fa-search"></a>
-												<a href="#" class="fa fa-link"></a>
-												<h4>Project title</h4>
+												<a href="https://farm8.staticflickr.com/7539/15624450653_f2b8eb346a_b_d.jpg" data-lightbox="image-1" class="fa fa-search"></a>
+												<a href="https://www.flickr.com/photos/leandrojiao/15624450653/" class="fa fa-link"></a>
+												<h4>Farewell Spit Tours</h4>
 												<p>Lorem ipsum dolor sit amet, consectetuer adipiscing elit, sed diam nonumm.</p>
 											</div>
 										</div>
 									</div>
 								</div>
-
-								<div class="iso-box graphic col-md-4 col-sm-6 col-xs-12">
+								<!--4-->
+								<div class="iso-box North_Island photoshop col-md-4 col-sm-6 col-xs-12">
 									<div class="photography-thumb">
-										<img src="images/photography/photography-img-4.jpg" class="fluid-img" alt="photography img">
+										<img src="https://farm8.staticflickr.com/7559/16161422482_88c7aeac52_n_d.jpg" class="fluid-img" alt="photography img">
 										<div class="photography-overlay">
 											<div class="photography-description">
-												<a href="#" data-lightbox="image-1" class="fa fa-search"></a>
-												<a href="#" class="fa fa-link"></a>
-												<h4>Project title</h4>
-												<p>Lorem ipsum dolor sit amet, consectetuer adipiscing elit, sed diam nonumm.</p>
+												<a href="https://farm8.staticflickr.com/7559/16161422482_88c7aeac52_b_d.jpg" data-lightbox="image-1" class="fa fa-search"></a>
+												<a href="https://www.flickr.com/photos/leandrojiao/16161422482/in/datetaken/" class="fa fa-link"></a>
+												<h4>Auckland Museum</h4>
+												<p>It's just so beautiful, even it's not any historical relic.</p>
 											</div>
 										</div>
 									</div>
 								</div>
-
-								<div class="iso-box wallpaper col-md-4 col-sm-6 col-xs-12">
+								<!--5 Middle -->
+								<div class="iso-box Christchurch col-md-4 col-sm-6 col-xs-12">
 									<div class="photography-thumb">
-										<img src="images/photography/photography-img-5.jpg" class="fluid-img" alt="photography img">
+										<img src="https://farm6.staticflickr.com/5484/14179832240_993cb9e5be_n_d.jpg" class="fluid-img" alt="photography img">
 										<div class="photography-overlay">
 											<div class="photography-description">
-												<a href="#" data-lightbox="image-1" class="fa fa-search"></a>
-												<a href="#" class="fa fa-link"></a>
-												<h4>Project title</h4>
-												<p>Lorem ipsum dolor sit amet, consectetuer adipiscing elit, sed diam nonumm.</p>
+												<a href="https://farm6.staticflickr.com/5484/14179832240_993cb9e5be_b_d.jpg" data-lightbox="image-1" class="fa fa-search"></a>
+												<a href="https://www.flickr.com/photos/leandrojiao/14179832240/" class="fa fa-link"></a>
+												<h4>Christchurch City</h4>
+												<p>2014, Christchurch, a city that even earthquake it couldn't make me want to leave.</p>
 											</div>
 										</div>
 									</div>
 								</div>
-								<div class="iso-box graphic photoshop col-md-4 col-sm-6 col-xs-12">
+								<!--6-->
+								<div class="iso-box Christchurch photoshop col-md-4 col-sm-6 col-xs-12">
 									<div class="photography-thumb">
-										<img src="images/photography/photography-img-6.jpg" class="fluid-img" alt="photography img">
+										<img src="https://farm8.staticflickr.com/7381/12852649375_dc2e54588a_n_d.jpg" class="fluid-img" alt="photography img">
 										<div class="photography-overlay">
 											<div class="photography-description">
-												<a href="#" data-lightbox="image-1" class="fa fa-search"></a>
-												<a href="#" class="fa fa-link"></a>
-												<h4>Project title</h4>
+												<a href="https://farm8.staticflickr.com/7381/12852649375_dc2e54588a_b_d.jpg" data-lightbox="image-1" class="fa fa-search"></a>
+												<a href="https://www.flickr.com/photos/leandrojiao/12852649375/in/datetaken/" class="fa fa-link"></a>
+												<h4>Spring of Christchurch</h4>
+												<p>Unknown flower name.</p>
+											</div>
+										</div>
+									</div>
+								</div>
+								<!--7-->
+								<div class="iso-box Christchurch photoshop col-md-4 col-sm-6 col-xs-12">
+									<div class="photography-thumb">
+										<img src="https://farm4.staticflickr.com/3805/13912689822_67dafc6df4_n_d.jpg" class="fluid-img" alt="photography img">
+										<div class="photography-overlay">
+											<div class="photography-description">
+												<a href="https://farm4.staticflickr.com/3805/13912689822_67dafc6df4_b_d.jpg" data-lightbox="image-1" class="fa fa-search"></a>
+												<a href="https://www.flickr.com/photos/leandrojiao/13912689822/" class="fa fa-link"></a>
+												<h4>Port Hill</h4>
+												<p>Friends of mine.</p>
+											</div>
+										</div>
+									</div>
+								</div>
+								<!--8-->
+								<div class="iso-box North_Island photoshop col-md-4 col-sm-6 col-xs-12">
+									<div class="photography-thumb">
+										<img src="https://farm4.staticflickr.com/3849/15081273326_1e62ff74bb_n_d.jpg" class="fluid-img" alt="photography img">
+										<div class="photography-overlay">
+											<div class="photography-description">
+												<a href="https://farm4.staticflickr.com/3849/15081273326_1e62ff74bb_b_d.jpg" data-lightbox="image-1" class="fa fa-search"></a>
+												<a href="https://www.flickr.com/photos/leandrojiao/15081273326/" class="fa fa-link"></a>
+												<h4>Auckland</h4>
+												<p></p>
+											</div>
+										</div>
+									</div>
+								</div>
+								<!--9-->
+								<div class="iso-box Christchurch photoshop col-md-4 col-sm-6 col-xs-12">
+									<div class="photography-thumb">
+										<img src="https://farm4.staticflickr.com/3816/12671283003_e5cca61d49_n_d.jpg" class="fluid-img" alt="photography img">
+										<div class="photography-overlay">
+											<div class="photography-description">
+												<a href="https://farm4.staticflickr.com/3816/12671283003_e5cca61d49_b_d.jpg" data-lightbox="image-1" class="fa fa-search"></a>
+												<a href="https://www.flickr.com/photos/leandrojiao/12671283003/" class="fa fa-link"></a>
+												<h4>Neighbour's kitten</h4>
+												<p>Bit curious of what is she thinking at that moment.</p>
+											</div>
+										</div>
+									</div>
+								</div>
+								<!--10-->
+								<div class="iso-box North_Island photoshop col-md-4 col-sm-6 col-xs-12">
+									<div class="photography-thumb">
+										<img src="https://farm4.staticflickr.com/3869/14639428971_21411cd3db_n_d.jpg" class="fluid-img" alt="photography img">
+										<div class="photography-overlay">
+											<div class="photography-description">
+												<a href="https://farm4.staticflickr.com/3869/14639428971_21411cd3db_b_d.jpg" data-lightbox="image-1" class="fa fa-search"></a>
+												<a href="https://www.flickr.com/photos/leandrojiao/14639428971/" class="fa fa-link"></a>
+												<h4>Hugs at Auckland</h4>
+												<p>It was a real big hug, just pass to you so much energy to being possitive.</p>
+											</div>
+										</div>
+									</div>
+								</div>
+								<!--11-->
+								<div class="iso-box North_Island photoshop col-md-4 col-sm-6 col-xs-12">
+									<div class="photography-thumb">
+										<img src="https://farm6.staticflickr.com/5574/14456080098_e7bdfbc51e_n_d.jpg" class="fluid-img" alt="photography img">
+										<div class="photography-overlay">
+											<div class="photography-description">
+												<a href="https://farm6.staticflickr.com/5574/14456080098_e7bdfbc51e_b_d.jpg" data-lightbox="image-1" class="fa fa-search"></a>
+												<a href="https://www.flickr.com/photos/leandrojiao/14456080098/" class="fa fa-link"></a>
+												<h4>Queen St @ Auckland</h4>
+												<p></p>
+											</div>
+										</div>
+									</div>
+								</div>
+								<!--12-->
+								<div class="iso-box South_Island photoshop col-md-4 col-sm-6 col-xs-12">
+									<div class="photography-thumb">
+										<img src="https://farm9.staticflickr.com/8676/15766225499_600f71196c_n_d.jpg" class="fluid-img" alt="photography img">
+										<div class="photography-overlay">
+											<div class="photography-description">
+												<a href="https://farm9.staticflickr.com/8676/15766225499_600f71196c_b_d.jpg" data-lightbox="image-1" class="fa fa-search"></a>
+												<a href="https://www.flickr.com/photos/leandrojiao/15766225499/" class="fa fa-link"></a>
+												<h4>West Coast</h4>
+												<p>Hiking &amp; Kayaking Trip with Rita.</p>
+											</div>
+										</div>
+									</div>
+								</div>
+								<!--13-->
+								<div class="iso-box South_Island photoshop col-md-4 col-sm-6 col-xs-12">
+									<div class="photography-thumb">
+										<img src="https://farm8.staticflickr.com/7516/15764935420_d46089a5d4_n_d.jpg" class="fluid-img" alt="photography img">
+										<div class="photography-overlay">
+											<div class="photography-description">
+												<a href="https://farm8.staticflickr.com/7516/15764935420_d46089a5d4_b_d.jpg" data-lightbox="image-1" class="fa fa-search"></a>
+												<a href="https://www.flickr.com/photos/leandrojiao/15764935420/" class="fa fa-link"></a>
+												<h4>Pancake Rock</h4>
+												<p>Rainbow at Pancake Rock, We just at the right time.</p>
+											</div>
+										</div>
+									</div>
+								</div>
+								<!--14-->
+								<div class="iso-box North_Island col-md-4 col-sm-6 col-xs-12">
+									<div class="photography-thumb">
+										<img src="https://farm8.staticflickr.com/7498/16161421602_626684d51f_n_d.jpg" class="fluid-img" alt="photography img">
+										<div class="photography-overlay">
+											<div class="photography-description">
+												<a href="https://farm8.staticflickr.com/7498/16161421602_626684d51f_b_d.jpg" data-lightbox="image-1" class="fa fa-search"></a>
+												<a href="https://www.flickr.com/photos/leandrojiao/16161421602/" class="fa fa-link"></a>
+												<h4>Sushi at Auckland</h4>
+												<p>2014, trip to Auckland, catch up with a friend at a Sushi Shop. Randomly panning shooting a car.</p>
+											</div>
+										</div>
+									</div>
+								</div>
+								<!--15-->
+								<div class="iso-box North_Island photoshop col-md-4 col-sm-6 col-xs-12">
+									<div class="photography-thumb">
+										<img src="https://farm4.staticflickr.com/3898/14917580009_76d3d99a6c_n_d.jpg" class="fluid-img" alt="photography img">
+										<div class="photography-overlay">
+											<div class="photography-description">
+												<a href="https://farm4.staticflickr.com/3898/14917580009_76d3d99a6c_b_d.jpg" data-lightbox="image-1" class="fa fa-search"></a>
+												<a href="https://www.flickr.com/photos/leandrojiao/14917580009/in/datetaken/" class="fa fa-link"></a>
+												<h4>Piha Beach</h4>
 												<p>Lorem ipsum dolor sit amet, consectetuer adipiscing elit, sed diam nonumm.</p>
 											</div>
 										</div>
